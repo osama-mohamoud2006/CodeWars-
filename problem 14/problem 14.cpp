852f30abcc2faeff9194f2207d0c64b28390be8a
osama mohamoud <wwwhdosama@gmail.com>
2025-07-24 16:35:38 +0300

    Auto commit: 2025-07-24 16:35:37

--- problem 14/problem 14.cpp
+++ problem 14/problem 14.cpp
@@ -17,13 +17,21 @@ string string_swap(int a, int b, string str) {
 	string result = "";
 	for (int i = 0; i < str.length(); i++) {
 		
+		// if i == the start index
 		if (i == a ) {
-			if (b >= str.length()) b = str.length() - 1;
+
+			if (b >= str.length()) b = str.length() - 1; // to ensure that the b isn't 
+			//bigger than actual length 
+
 			for (int s = b; s >= a; s--) {
+				// add char inversely from the end index to start index 
+				// b is End
+				// a is Start ( both of them according to codewars problem)
 				result += str[s];
 				
 			}
-			i = b;
+			i = b; // after finishing make i = the end to ensure add the rest of chars correctly
+			// to the resukt and avoiding unneccessary repetion of chars 
 		}
 		
 		else
