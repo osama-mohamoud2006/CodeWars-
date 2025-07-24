c5b6b107bf2cf6deeec90dae6ff775b3ef53ce5d
osama mohamoud <wwwhdosama@gmail.com>
2025-07-24 20:54:55 +0300

    Auto commit: 2025-07-24 20:54:54

--- problem 13/problem 13.cpp
+++ problem 13/problem 13.cpp
@@ -1,70 +1,42 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 using namespace std;
-/*
-For example:
 
-"our code" => "edo cruo"
--- Normal reversal without spaces is "edocruo".
--- However, there is a space at index 3, so the string becomes "edo cruo"
+string reverse_preserve_spaces(string str) {
+    string only_chars = "";
 
-"your code rocks" => "skco redo cruoy".
-"codewars" => "srawedoc"
-
-
-*/
-string input() {
-	string str;
-	cout << "enter the string: ";
-	getline(cin, str);
-	return  str;
- }
-
-void fill_index(int number, int arr[100], int& l2) {
-	l2++;
-	arr[l2 - 1] = number;
-
-}
+    // Step 1: Get all characters without spaces
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] != ' ')
+            only_chars += str[i];
+    }
 
 
-void fill_array_with_index(string str , int arr[100] , int &l2) {
+    // Step 2: Reverse only the characters
+    reverse(only_chars.begin(), only_chars.end());
 
-	int index_to_pass = 0; 
-	for (int index = 0; index < str.length(); index++) {
-		// to get the inderx od first word
-		if (str[index] != ' ') index_to_pass++;
-		else {
-			fill_index(index_to_pass, arr, l2);
-		} // 4 , 7 , 10 
-	}
+    // Step 3: Fill result keeping spaces in same positions
+    string result = "";
+    int char_index = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ')
+            result += ' ';
+        else {
+            result += only_chars[char_index];
+            char_index++;
+        }
+    }
 
-
-	
-}
-
-void string_with_og_index(string str, int arr[100], int l2) {
-	string result = "";
-	for (int in = 0; in < l2; in++) {
-		// 4 
-
-		for (int g = str.length()-1; g >= 0; g--) {
-			
-			int count = arr[in]; // 4
-			if (count != 0)result += str[g];
-			count--;
-
-		}
-
-
-	}
-	cout << "the result: " << result << endl;
-
+    return result;
 }
 
 int main() {
-	int arr[100];
-	int l2 = 0; 
-	string str = input();
-	fill_array_with_index(str, arr, l2);
-	string_with_og_index(str, arr, l2);
+    string s;
+    cout << "Enter the string: ";
+    getline(cin, s);
+
+    string res = reverse_preserve_spaces(s);
+    cout << "Result: " << res << endl;
+
+    return 0;
 }
