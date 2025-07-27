024b2e218a2c1995f5fd7cfe31c951d00e0ef96d
osama mohamoud <wwwhdosama@gmail.com>
2025-07-27 15:53:58 +0300

    Auto commit: 2025-07-27 15:53:57

---
+++ problem 16/problem 16.cpp
@@ -0,0 +1,14 @@
+#include"E:\projects\my library\AllStuff.h"   
+#include <iostream>
+using namespace std;
+using namespace AllStuff;
+
+void remove_double(string input) {
+	// doubles('a bb ccc dddd a') = 'aca'
+	 //  double is even numbers , we will 
+
+}
+
+int main() {
+	string input = read_string("enter the string: ");
+}
