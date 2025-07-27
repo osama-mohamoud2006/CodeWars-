c9a2a37c6a7dafa9711ff5a20d74be599435e4da
osama mohamoud <wwwhdosama@gmail.com>
2025-07-27 23:28:41 +0300

    Auto commit: 2025-07-27 23:28:41

--- problem 18/problem 18.cpp
+++ problem 18/problem 18.cpp
@@ -1,30 +1,28 @@
-//#include<iostream>
-//#include"E:\projects\my library\AllStuff.h"  
-//using namespace AllStuff;
-//using namespace std;
-//const int size = 5; 
-//int  save(int arr[::size], int number ) {
-//	int sum = 0;
-//	int count = 0; 
-//	for (int i = 0; i < ::size; i++) {
-//		sum += arr[i]; // 4
-//		if (sum > number) break;
-//		count++;
-//	}
-//	return count; 
-//}
-//
-//int main() {
-//	int arr[::size];
-//	array_input(arr, ::size);
-//	cout << "\n";
-//	int n = enter_number("enter HD free size: ");
-//	cout << '\n';
-//	cout<<"the number of files you can save is: "<<save(arr, n)<<endl;
-//
-//
-//	
-//
-//
-//}
+#include<iostream>
+#include"E:\projects\my library\AllStuff.h"  
+using namespace AllStuff;
+using namespace std;
+const int size = 5; 
+int  save(int arr[::size], int number ) {
+	int sum = 0;
+	int count = 0; 
+	for (int i = 0; i < ::size; i++) {
+		sum += arr[i]; // 4
+		if (sum > number) break;
+		count++;
+	}
+	return count; 
+}
 
+int main() {
+	int arr[::size];
+	array_input(arr, ::size);
+	cout << "\n";
+	int n = enter_number("enter HD free size: ");
+	cout << '\n';
+	cout<<"the number of files you can save is: "<<save(arr, n)<<endl;
+
+
+
+
+}
