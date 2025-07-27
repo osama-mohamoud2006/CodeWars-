18e61887486fd30fbfaa98ea207208db287587b0
osama mohamoud <wwwhdosama@gmail.com>
2025-07-27 17:54:57 +0300

    Auto commit: 2025-07-27 17:54:56

--- problem 16/problem 16.cpp
+++ problem 16/problem 16.cpp
@@ -1,28 +1,19 @@
-#include"E:\projects\my library\AllStuff.h"   
+#include "E:\\projects\\my library\\AllStuff.h"   
 #include <iostream>
 using namespace std;
 using namespace AllStuff;
 
-string remove_double(string input) {
-	// doubles('a bb ccc dddd a') = 'aca'
-	 //  double is even numbers , we will remove it
-	string result = "";
-	for (int s = 0; s < input.length(); s++) {
-		int count = 0; 
-		// take the first letter  a
-		for (int l = 0; l < input.length(); l++) { //1+1
-		
-			 (input[s] == input[l])? count++ :count+=0;
-			 
-		}
-		if (result.find(input[s]) == string::npos)
+///  a bb ccc dddd a
 
-	(is_odd(count) == true) ? result += input[s] : result;
-	}
-	return result; 
+string remove_double(string input) {
+	string result = "";
+	for (char current_char : input)
+		(result.empty() || result.back() != current_char) ? result.push_back(current_char) : result.pop_back();
+	return result;
 }
+
 
 int main() {
 	string input = read_string("enter the string: ");
-	cout<<"\n"<<remove_double(input) << endl;
+	cout << "\n" << remove_double(input) << endl;
 }
