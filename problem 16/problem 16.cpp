53f06428c1384b51d6892af491f202b6de89507f
osama mohamoud <wwwhdosama@gmail.com>
2025-07-27 16:05:09 +0300

    Auto commit: 2025-07-27 16:05:08

--- problem 16/problem 16.cpp
+++ problem 16/problem 16.cpp
@@ -5,7 +5,16 @@ using namespace AllStuff;
 
 void remove_double(string input) {
 	// doubles('a bb ccc dddd a') = 'aca'
-	 //  double is even numbers , we will 
+	 //  double is even numbers , we will remove it
+	string result = "";
+	for (int s = 0; s < input.length(); s++) {
+		int count = 0; 
+		// take the first letter  a
+		for (char l :  input) {
+			 (input[s] == input[l])? count++ : count+=0;
+		}
+		(is_even(count) == true) ? result += input[s] : ;
+	}
 
 }
 
