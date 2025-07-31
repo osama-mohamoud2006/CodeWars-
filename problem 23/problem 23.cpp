c7ec238ce1feb78691dc0b074789270ffbe21248
osama mohamoud <wwwhdosama@gmail.com>
2025-07-31 21:03:51 +0300

    Auto commit: 2025-07-31 21:03:50

--- problem 23/problem 23.cpp
+++ problem 23/problem 23.cpp
@@ -1,15 +1,22 @@
 // if A (- B --> return true
 #include <iostream>
+#include <string>
 using namespace std;
+
+
 
 bool is_string_belongingto(string a , string b ){
     // abc  --- > bc 
-         bool v =false;  
-    for(char c :a ){
-        // a
-        for(char c2:b){
-             
-        }
+        for(int i=0; i<b.length(); i++){
+           ///c d e
+           //c 
+           for(int i2=0; i<a.length(); i2++){
 
-    }
+           }
+
+        }
+}
+
+int main(){
+    is_string_belongingto("abc" ,"bc" );
 }
