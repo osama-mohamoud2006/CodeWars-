b5911c3dc723030f4527059a1c8bfe5df2581ac4
osama mohamoud <wwwhdosama@gmail.com>
2025-07-31 21:07:06 +0300

    Auto commit: 2025-07-31 21:07:05

--- problem 23/problem 23.cpp
+++ problem 23/problem 23.cpp
@@ -7,16 +7,24 @@ using namespace std;
 
 bool is_string_belongingto(string a , string b ){
     // abc  --- > bc 
-        for(int i=0; i<b.length(); i++){
+    bool v = false; 
+    for(int i=0; i<b.length(); i++){
            ///c d e
-           //c 
+           //d
            for(int i2=0; i<a.length(); i2++){
-
+                 // a b c d e 
+                 if(b[i] == a[i2] ){
+                    v = true;
+                    break;
+                 }
            }
 
         }
+
+        if(v == true) return true ; 
 }
 
+
 int main(){
-    is_string_belongingto("abc" ,"bc" );
+ cout<<  is_string_belongingto("abcde" ,"cde" )<<endl;
 }
