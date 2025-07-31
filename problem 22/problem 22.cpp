432f6846bf2ce723fb30f56594ad0163a8d4724d
osama mohamoud <wwwhdosama@gmail.com>
2025-07-31 20:11:12 +0300

    Auto commit: 2025-07-31 20:11:12

---
+++ problem 22/problem 22.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std; 
+//vowels are "a", "e", "i", "o", "u"
+
+
+// function declration 
+string remove_vowel(string str="" , int c=0);
+
+int main(){
+    cout<<"\n"<<remove_vowel("aeiou")<<"\n";
+}
+
+                 ///// first method ../////////
+//  string remove_vowel(string str){
+//  string without = "";
+//  for(char c : str){
+//     if(c!='a' &&c!='e' &&c!='i'&&c!='o' && c!='u') without+=c;
+//  }
+//   return without ; 
+ 
+//  }
+
+//  Recursion  method 
+  string remove_vowel(string str , int c){
+  static string without = ""; // to store the res and not intilize each time 
+   if(c== str.length()) return without; // base case
+   else{
+
+    if(str[c]!='a' && str[c]!='e' &&str[c]!='i'&&str[c]!='o' && str[c]!='u') without+=str[c];
+ 
+            // c +1 to increment the index to access the next char 
+     return remove_vowel( str ,  c+1) ;  // call function again recrusivly 
+   }
+   
+
+ }
--- problem21/problem 21.cpp
+++ problem21/problem 21.cpp
@@ -2,26 +2,6 @@
 #include <iostream>
 using namespace std;
 
-/*
-    dotest("()",                 true);
-    dotest("((()))",             true);
-    dotest("()()()",             true);
-    dotest("(()())()",           true);
-    dotest("()(())((()))(())()", true);
-  }
-*/
-
-/*
-  It(InvalidCases) {
-    dotest(")(",     false);
-    dotest("()()(",  false);
-    dotest("((())",  false);
-    dotest("())(()", false);
-    dotest(")()",    false);
-    dotest(")",      false);
-  }
-*/
-
 bool validParentheses( string str) {
 
  int ok =0 ;
@@ -32,9 +12,6 @@ bool validParentheses( string str) {
  if(ok<0)  return false;  
     
   }
-
- 
-  
 
   return !ok ;
 }
