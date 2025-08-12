9c0f85a9cdeff0cf48e1e30cb2a691e937eee722
osama mohamoud <wwwhdosama@gmail.com>
2025-08-12 18:48:16 +0300

    Auto commit: 2025-08-12 18:48:16

--- problem 27/problem 27.cc
+++ problem 27/problem 27.cc
@@ -13,7 +13,6 @@ string vaporcode(const string &str) {
   for(auto c: str ){
     if(c!=' ') original+=c;
   }
-  //cout<<original<<endl;
 
   string vap ="";
   for(auto i: original ){
@@ -31,4 +30,15 @@ string vaporcode(const string &str) {
 int main(){
     
   cout<<  vaporcode("Let's go to the movies")<<endl; // "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+
+  string test =" hello wwold ";
+  cout<<"\nbefore eraSE:"<<test<<endl;
+ 
+  test.erase(0,1); // pos,count 
+    cout<<"after eraSE1:"<<test<<endl;
+    test.erase(1,2);// remove chars start from h (will not remove) and remove 2 chars 
+       cout<<"after eraSE1:"<<test<<endl;
+
+        test.erase(1);// remove chars start from h(index:1) (will not remove) and remove all chars after the start  
+         cout<<"after eraSE2:"<<test<<endl;
 }
